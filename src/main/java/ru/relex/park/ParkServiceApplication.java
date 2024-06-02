96e91ad2e9cd0e863fcdd9edd4c282396ac312e8
Andrey <andrejantonov63@gmail.com>
2024-06-02 12:50:17 +0300

    Заготовка проекта для сервиса

---
+++ src/main/java/ru/relex/park/ParkServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.relex.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/relex/park/ParkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.relex.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
